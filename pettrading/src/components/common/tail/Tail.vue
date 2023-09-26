01c56eea78613efa47b7982376a9d034e14e534a
XinChennn~ <316595344@qq.com>
2023-09-26 14:38:01 +0800

    [忻辰]first commit

---
+++ pettrading/src/components/common/tail/Tail.vue
@@ -0,0 +1,35 @@
+<!---->
+<template>
+  <div class='tail'>
+    <div class="mainbar container">
+      <div>
+        Created by <span style="color: aqua">XinChen</span>, find me by <a href="https://www.ixinjiu.cn" style="color: cornflowerblue">https://www.ixinjiu.cn</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Tail',
+    data () {
+      return {}
+    }
+  }
+</script>
+<style scoped>
+@import "~assets/css/mediacss.css";
+
+.tail {
+  width: 100%;
+  min-height: 5em;
+  margin-top: 10em;
+  background: rgb(39, 39, 39);
+  color: rgb(156, 156, 156);
+}
+
+.mainbar {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
